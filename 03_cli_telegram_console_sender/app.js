e0f07c09db77b42fb419ab72b9a6079f6d56b5e1
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-10-22 10:58:59 +0300

    refactor app.js

--- 03_cli_telegram_console_sender/app.js
+++ 03_cli_telegram_console_sender/app.js
@@ -1,4 +1,5 @@
 import { program } from "commander";
+import { readFileSync, writeFileSync } from "fs";
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const packageInfo = require("./package.json");
@@ -9,25 +10,64 @@ program
         "CLI tool for saving messages and photos through Telegram bot"
     );
 
-program
-    .command("send-message")
-    .description("Send message to telegram bot")
-    .alias("m")
-    .argument("<message>", "Message to send")
-    .action((message) => {
-        console.log("Your message ", message);
-    });
+async function readConfig() {
+    const botTokenPath = new URL(`./bot_token.txt`, import.meta.url);
+    const userIdPath = new URL(`./user_id.txt`, import.meta.url);
+    let botToken, userId;
+    try {
+        botToken = readFileSync(botTokenPath, "utf-8");
+    } catch (e) {
+        writeFileSync(botTokenPath, "");
+    }
+    if (!botToken) {
+        console.log(
+            "Bot token is not installed.\nPlease paste your bot token into file bot_token.txt that was generated in the folder"
+        );
+        process.exit(1);
+    }
+    try {
+        userId = readFileSync(userIdPath, "utf-8");
+    } catch (e) {
+        writeFileSync(userIdPath, "");
+    }
+    userId = null;
+    return [botToken, userId];
+}
 
-program
-    .command("send-photo")
-    .description(
-        "Send photo to telegram bot. Just drag and drop it after writing the command"
-    )
-    .alias("p")
-    .argument("<path>", "Path to photo")
-    .action((photoPath) => {
-        console.log("Path to photo", photoPath);
-        console.log(options);
-    });
+async function startAction() {
+    const [botToken, userId] = await readConfig();
+    console.log(
+        "Start the bot using link below.\nProcess will exit in 1 minute if no action is done or if error occurs"
+    );
+    //To do
+}
+async function sendMessageAction(message) {
+    const [botToken, userId] = await readConfig();
+    //To do
+}
+async function sendPhotoAction(photoPath) {
+    const [botToken, userId] = await readConfig();
+    //To do
+}
 
-program.parse();
+async function main() {
+    program.command("start").description(`Start the bot.`).action(startAction);
+    program
+        .command("send-message")
+        .description("Send message to telegram bot")
+        .alias("m")
+        .argument("<message>", "Message to send")
+        .action(sendMessageAction);
+
+    program
+        .command("send-photo")
+        .description(
+            "Send photo to telegram bot. Just drag and drop it after writing the command"
+        )
+        .alias("p")
+        .argument("<path>", "Path to photo")
+        .action(sendPhotoAction);
+
+    program.parse();
+}
+main();
