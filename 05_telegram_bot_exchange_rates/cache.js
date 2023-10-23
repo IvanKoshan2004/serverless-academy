299adf1aed733520fca207756c244a7b3d2b529a
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-10-23 18:23:42 +0300

    created a cache with time expiration module

---
+++ 05_telegram_bot_exchange_rates/cache.js
@@ -0,0 +1,40 @@
+export class Cache {
+    constructor(options = { wipeInterval: 270 }) {
+        this._map = new Map();
+        setInterval(this.wipeExpiredCache, options.wipeInterval * 1000);
+    }
+
+    get(key) {
+        const object = this._map.get(key);
+        if (object == undefined) return null;
+        if (object.expires < Date.now()) {
+            this.delete(key);
+            return null;
+        }
+        return object.data;
+    }
+    set(
+        key,
+        object,
+        options = {
+            ttls: 60,
+        }
+    ) {
+        const cacheObject = {
+            expires: options.ttls * 1000 + Date.now(),
+            data: object,
+        };
+        this._map.set(key, cacheObject);
+    }
+    delete(key) {
+        this._map.delete(key);
+    }
+    wipeExpiredCache() {
+        const now = Date.now();
+        this._map.forEach((val, key) => {
+            if (val.expires < now) {
+                this.delete(key);
+            }
+        });
+    }
+}
