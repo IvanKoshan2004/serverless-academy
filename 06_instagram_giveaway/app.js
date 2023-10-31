db9478845fff3db5cd9e0118b75fc38d843b61fc
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-10-31 21:24:02 +0200

    solution fix

--- 06_instagram_giveaway/app.js
+++ 06_instagram_giveaway/app.js
@@ -29,11 +29,12 @@ function existInAllFiles() {
     for (let i = 0; i < 20; i++) {
         const fileContent = readFileSync(`./out${i}.txt`, "utf-8");
         const array = fileContent.split("\n");
-        for (let j = 0; j < array.length; j++) {
-            let currCount = map.get(array[j]);
+        const set = new Set(array);
+        set.forEach((el) => {
+            let currCount = map.get(el);
             currCount = currCount === undefined ? 0 : currCount;
-            map.set(array[j], currCount + 1);
-        }
+            map.set(el, currCount + 1);
+        });
     }
     let count = 0;
     map.forEach((el) => {
@@ -46,11 +47,12 @@ function existInAtleastTen() {
     for (let i = 0; i < 20; i++) {
         const fileContent = readFileSync(`./out${i}.txt`, "utf-8");
         const array = fileContent.split("\n");
-        for (let j = 0; j < array.length; j++) {
-            let curr = map.get(array[j]);
-            curr = curr === undefined ? 0 : curr;
-            map.set(array[j], curr + 1);
-        }
+        const set = new Set(array);
+        set.forEach((el) => {
+            let currCount = map.get(el);
+            currCount = currCount === undefined ? 0 : currCount;
+            map.set(el, currCount + 1);
+        });
     }
     let count = 0;
     map.forEach((el) => {
