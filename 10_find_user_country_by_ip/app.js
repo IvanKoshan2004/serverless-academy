fc58263f8d05414954fe3391b8becae9342c1f54
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-04 18:41:08 +0200

    integrate ipLocator into the app

--- 10_find_user_country_by_ip/app.js
+++ 10_find_user_country_by_ip/app.js
@@ -1,19 +1,27 @@
 import express from "express";
-
-const app = express();
-app.set("trust proxy", true);
+import { IpLocator } from "./ip-locator.js";
 
 const PORT = process.env.PORT || 3000;
-app.get("", (req, res) => {
-    const userIp = req.ip;
-    const userCountry = "country";
-    const ipRangeStart = "0.0.0.0";
-    const ipRangeEnd = "255.255.255.255";
+const IP_RANGES_CSV_FILE = "IP2LOCATION-LITE-DB1.CSV"; // should be in project root
 
-    res.status(200);
-    res.send(
-        `Hello, your ip is ${userIp}, you live in ${userCountry}.\nIp range: ${ipRangeStart} - ${ipRangeEnd}`
-    );
+const app = express();
+const ipLocator = new IpLocator();
+const ipRangesCsvPath = new URL(`./${IP_RANGES_CSV_FILE}`, import.meta.url);
+await ipLocator.initialize(ipRangesCsvPath);
+
+app.set("trust proxy", true);
+app.get("", (req, res) => {
+    try {
+        const userIp = req.ip;
+        const ipData = ipLocator.findIpData(userIp);
+        res.status(200);
+        res.send(
+            `Hello, your ip is ${userIp}, you live in ${ipData.countryName}.\nIp range: ${ipData.rangeStart} - ${ipData.rangeEnd}`
+        );
+    } catch (e) {
+        res.status(500);
+        res.send("Sorry we couldn't process your ip :(");
+    }
 });
 
 app.listen(PORT, () => {
