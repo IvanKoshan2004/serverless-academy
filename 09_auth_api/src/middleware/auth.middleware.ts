7fdbb8a5906029a99283475f284688b07974d156
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-04 10:52:26 +0200

    implemented authentication middleware

--- 09_auth_api/src/middleware/auth.middleware.ts
+++ 09_auth_api/src/middleware/auth.middleware.ts
@@ -2,7 +2,26 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import { IUserModel } from "../db/interfaces/user-model.interface";
 
 export function createAuthenticateUserMiddleware(userModel: IUserModel) {
-    return (req: Request, res: Response, next: NextFunction) => {
+    function extractToken(authHeader: string): string {
+        const authHeaderSplit = authHeader.split(" ");
+        if (authHeaderSplit[0] === "Bearer") {
+            return authHeaderSplit[1];
+        }
+        return "";
+    }
+    return async (req: Request, res: Response, next: NextFunction) => {
+        const authHeader = req.headers["authorization"];
+        if (authHeader === undefined) {
+            res.send({ status: false, error: "User not authorized" });
+            return;
+        }
+        const token = extractToken(authHeader);
+        const user = await userModel.verifyJwt(token);
+        if (user === null) {
+            res.send({ status: false, error: "Invalid token" });
+            return;
+        }
+        req.user = user;
         next();
     };
 }
