7a4e1fa22f175edd98fd121f96a67ec6daa4304e
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-05 20:21:47 +0200

    implemented the application logic

--- 11_json_storage/app.js
+++ 11_json_storage/app.js
@@ -3,6 +3,7 @@ import { resolve } from "path";
 import { fileURLToPath } from "url";
 import { DBCore } from "./key-value-db.js";
 import { jsonBodyParser } from "./json-body-parser.js";
+import { randomBytes } from "crypto";
 
 const PORT = process.env.PORT || 3000;
 const JSON_ROUTE = "/json";
@@ -14,23 +15,43 @@ const dbFolderPath = resolve(__dirname, DB_FOLDER_NAME);
 const app = new Koa();
 const db = new DBCore(dbFolderPath);
 await db.initDb();
-app.use(jsonBodyParser);
 
-app.use((ctx) => {
+app.use(jsonBodyParser);
+app.use(async (ctx) => {
     if (ctx.path == JSON_ROUTE) {
         switch (ctx.method) {
-            case "GET":
-                ctx.body = { hello: new Date() };
-                break;
-            case "PUT":
-                console.log(ctx.req.body);
-                ctx.body = "found";
-                break;
+            case "GET": {
+                const { key } = ctx.query;
+                if (!key) {
+                    ctx.throw(400, "Set key query param to get the json");
+                }
+                try {
+                    const value = await db.get(key);
+                    ctx.set("Content-Type", "application/json");
+                    ctx.body = value;
+                } catch (e) {
+                    ctx.status = 500;
+                    ctx.body = "Sorry but we couldn't get your json";
+                }
+                return;
+            }
+            case "PUT": {
+                const key = randomBytes(5).toString("base64url");
+                const value = JSON.stringify(ctx.req.body);
+                try {
+                    await db.store(key, value);
+                    const serverUrl = ctx.request.URL;
+                    const link = `${serverUrl.protocol}//${serverUrl.host}${JSON_ROUTE}?key=${key}`;
+                    ctx.body = `Saved your json in the db. Get your json <a href="${link}">here</a>`;
+                } catch (e) {
+                    ctx.status = 500;
+                    ctx.body = "Sorry but we couldn't save your json";
+                }
+                return;
+            }
             default:
                 ctx.throw(405, "Method not allowed");
-                break;
         }
-        return;
     }
     ctx.throw(404, "Route not found");
 });
