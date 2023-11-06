5ec1d16c40fd03e272698dedfecc275360216418
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-06 21:29:26 +0200

    implemented create shortlink controller

--- 12_shortlinker_api/controllers/shortlink-url.controller.js
+++ 12_shortlinker_api/controllers/shortlink-url.controller.js
@@ -1,5 +1,17 @@
 export function createShortlinkUrlController(shortlinkService) {
-    return function shortlinkUrlController(req, res) {
-        res.send("create short link");
+    return async function shortlinkUrlController(req, res) {
+        const { link } = req.body;
+        if (link === undefined) {
+            res.status(400);
+            res.send({
+                status: false,
+                error: "Body must contain link attribute",
+            });
+            return;
+        }
+        const tag = await shortlinkService.createShortLink(link);
+        res.status(201);
+        const shortlink = `${req.protocol}://${req.headers.host}/${tag}`;
+        res.send({ status: true, link: shortlink });
     };
 }
