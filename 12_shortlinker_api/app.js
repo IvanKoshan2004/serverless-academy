73420130347dc922d5da63e8b2bd68ff385fcf13
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-06 21:14:49 +0200

    created env, and configured db connection

--- 12_shortlinker_api/app.js
+++ 12_shortlinker_api/app.js
@@ -1,20 +1,24 @@
 import express from "express";
 import { createAppRouter } from "./router.js";
 import { ShortlinkService } from "./shortlink/shortlink.js";
+import { parseEnv } from "./parse-env.js";
 
 const PORT = process.env.PORT || 3000;
 const MAX_BYTES_PER_LINK_TAG = 3;
+parseEnv(".env");
 
 const app = express();
 const shortlinkService = new ShortlinkService({
     maxStoreBytes: MAX_BYTES_PER_LINK_TAG,
     mysqlConfig: {
-        host: "localhost",
-        user: "root",
-        password: "root",
-        database: "shortlink",
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
     },
 });
+await shortlinkService.initService();
+
 app.use(createAppRouter(shortlinkService));
 
 app.listen(PORT, () => {
