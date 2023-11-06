0e1afc4d4a1a51a314e1ae97f322a492c5cd1186
IvanKoshan2004 <ivan.koshangod@gmail.com>
2023-11-06 21:44:56 +0200

    implemented redirect controller

--- 12_shortlinker_api/controllers/redirect.controller.js
+++ 12_shortlinker_api/controllers/redirect.controller.js
@@ -1,5 +1,16 @@
-export function createRedirectController() {
-    return function redirectController(req, res) {
-        res.send("redirect");
+export function createRedirectController(shortlinkService) {
+    return async function redirectController(req, res) {
+        const { linkTag } = req.params;
+        if (linkTag === undefined) {
+            res.status(404);
+            res.send({ status: false, error: "Route doesn't exist" });
+        }
+        try {
+            const link = await shortlinkService.getLinkByTag(linkTag);
+            res.redirect(link);
+        } catch (e) {
+            res.status(404);
+            res.send({ status: false, error: "Short link doesn't exist" });
+        }
     };
 }
